c462e150d4bcbf2fb410a09e3d195ea0d109cdd3
Glitch (bright-patch) <none>
2018-09-13 00:41:33 +0000

    🌇👍 Checkpoint ./config/routes.rb:247484/205 ./app/controllers/main_controller.rb:247484/272 ./app/views/index.html.erb:247484/24

--- app/controllers/lights_controller.rb
+++
@@ -1,9 +0,0 @@
-class LightsController < ApplicationController
-  def get
-    
-  end
-  
-  def set
-    @client ||= ApiClient.new
-  end
-end
---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ApplicationController
+
+  def index
+    # Your code here
+  end
+
+end
