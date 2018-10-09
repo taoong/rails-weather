e9fb94b4e43ce50f5df6ffe75974e6dfc8b527f4
Glitch (full-year) <none>
2018-10-09 00:01:39 +0000

    🐋🏹 Checkpoint ./app/views/main/index.html.erb:1970176/3065 ./app/controllers/main_controller.rb:1970176/2316 ./app/models/city.rb:1970176/742

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,8 +3,19 @@ require_relative "../services/weather_service"
 class MainController < ApplicationController
 
   def index
-    # Uncomment and pass a parameter to the get function
-    # @w = WeatherService.get()
-  end
+    @w = WeatherService.get(params[:city])
+    if @w
+      # Bonus: Converting Kelvin to Fahrenheit
+      @temperature = (9 / 5) * (@w[:temperature] - 273) + 32
+      # Create a new city
+      city = City.new(
+        name: params[:city],
+        temperature: @temperature, # Using the converted temperature
+        description: @w[:description]
+      )
+      # Save the city so that it's in City.all
+      city.save
+    end
+  end  
 
 end
