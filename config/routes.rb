fed495a44bd5cffc18df47303c7cdb599bd977ea
Glitch (full-year) <none>
2018-10-10 05:34:05 +0000

    ❣️🐉 Checkpoint ./config/routes.rb:1970176/86

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "main#index"
-  get '/cities/view', to: 'pages#home'
+  get '/cities/view', to: 'cities#view'
+  get '/cities/new', to: 'cities#new'
+  post '/cities/signup
 end
