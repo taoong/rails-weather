851de6c14905671a7ef47ea353937963cf682621
Glitch (full-year) <none>
2018-10-11 02:15:31 +0000

    💠🖇 Checkpoint ./config/routes.rb:1970176/38 ./app/views/cities/new.html.erb:1970176/13 ./app/controllers/cities_controller.rb:1970176/5

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -11,6 +11,7 @@ class CitiesController < ApplicationController
   end
   
   def create
+    
   end
   
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "main#index"
   get '/cities/view', to: 'cities#view'
   get '/cities/new', to: 'cities#new'
-  post '/cities/create', to: 'cities#create'
+  post '/cities/create', to: 'cities#view'
   get 'cities/update', to: 'cities#update'
-  post 'cities/update', to: 'cities#update'
+  post 'cities/update', to: 'cities#view'
 end
